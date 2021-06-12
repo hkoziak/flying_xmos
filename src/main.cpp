70fadea693eb2fa561581bb54f9cc6da25fdbda1
Halyna Koziak <galiakozyak@gmail.com>
2021-06-12 09:15:45 +0300

    Rebuild of code, now it depends on mpu interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,10 @@
 
 MPU6050 mpu;
 
+bool debug = true;
+
 #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define DEBUG_PIN 7
 bool blinkState = false;
 
 // MPU control/status vars
@@ -43,65 +46,52 @@ PID pitchPID(&pitchInput, &pitchOutput, &setPitch, CoefP, CoefI, CoefD, DIRECT);
 PID rollPID(&rollInput, &rollOutput, &setRoll, CoefP, CoefI, CoefD, DIRECT);
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() 
-{
-    mpuInterrupt = true;
-}
-
-uint64_t lastMpuReaded = 0;
-const uint64_t MaxTimeWaitingForMpuData = 100;
 
 void readRcControllerData();
 bool isShutDownRequested();
 bool isMpuDataActual();
 void shutDownQ();
 void clearControlRequestValues();
-float limit_power(float p);
+void Generate_Motor_Pulses();
+void Update_Motor_Values();
+
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+
+    //Generate pulses
+    // Generate_Motor_Pulses();
+}
+
+uint64_t lastMpuReaded = 0;
+const uint64_t MaxTimeWaitingForMpuData = 100;
+
+int motor_upper_left_us = 0;
+int motor_upper_right_us = 0;
+int motor_lower_left_us = 0;
+int motor_lower_right_us = 0;
 
 void setup() 
 {
-    setupDriver();
+    setupDriver();  // Init motors with 0 value
 
-    Wire.begin();
-    TWBR = 24; // 400kHz I2C clock
+    Wire.begin();   // Init I2C for MPU6050
+    TWBR = 24;      // 400kHz I2C clock
 
     Serial.begin(115200); // Debug
-    Serial1.begin(9600);  // BLT
+    Serial1.begin(9600);  // Bluetooth
 
-
-// byte count = 0;
-// Wire.begin();
-// for (byte i = 8; i < 120; i++)
-// { Wire.beginTransmission (i);
-// if (Wire.endTransmission () == 0)
-// {
-// Serial.print ("Found address: ");
-// Serial.print (i, DEC);
-// Serial.print (" (0x");
-// Serial.print (i, HEX);
-// Serial.println (")");
-// count++;
-// delay (1);
-// } // end of good response
-// } // end of for loop
-
-    // initialize device
-    Serial.println(F("Initializing I2C devices..."));
+    // initialize MPU
+    if(debug) Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
 
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    delay(500);
-    // while (Serial.available() && Serial.read()); // empty buffer
-    // while (!Serial.available());                 // wait for data
-    // while (Serial.available() && Serial.read()); // empty buffer again
+    // verify connection to MPU
+    if(debug) Serial.println(F("Testing device connections..."));
+    if(debug) Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
+    if(debug) Serial.println(F("Initializing DMP..."));
+    delay(500); // do not remove, will crash
     devStatus = mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
@@ -115,34 +105,36 @@ void setup()
     mpu.setZGyroOffset(39);
 
     // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
+    if (devStatus == 0) 
+    {
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
+        if(debug) Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        if(debug) Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
         attachInterrupt(0, dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        if(debug) Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
+    } 
+    else 
+    {
         // ERROR!
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
+        if(debug) Serial.print(F("DMP Initialization failed (code "));
+        if(debug) Serial.print(devStatus);
+        if(debug) Serial.println(F(")"));
     }
 
-    // configure stabilization code
-    //=============================
+    // Init PID variables
     pitchOutput = 0.0;
     rollOutput = 0.0;
     setPitch = 0.0;
@@ -150,29 +142,127 @@ void setup()
     pitchInput = 0.0;
     rollInput = 0.0;
     
+    // Configure PID
     pitchPID.SetMode(AUTOMATIC);
     rollPID.SetMode(AUTOMATIC);   
     
     pitchPID.SetOutputLimits(-100, 100);
     rollPID.SetOutputLimits(-100, 100);
 
-    pinMode(13, OUTPUT);
-    pinMode(12, OUTPUT);
+    pinMode(DEBUG_PIN, OUTPUT);
+    digitalWrite(DEBUG_PIN, 0);
+    
+    // pinMode(8, OUTPUT);
+    // digitalWrite(8, 0);
+    // pinMode(9, OUTPUT);
+    // digitalWrite(9, 0);
+    // pinMode(10, OUTPUT);
+    // digitalWrite(10, 0);
+    // pinMode(11, OUTPUT);
+    // digitalWrite(11, 0);
 
+    mpu.setIntDataReadyEnabled(1);
+
+    Serial.print("getIntDataReadyEnabled: ");
+    Serial.println(mpu.getIntDataReadyEnabled());
+}
+
+unsigned long micros_time = 0;
+unsigned long micros_time_base = 0;
+
+void Generate_Motor_Pulses()
+{
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, HIGH);
+
+    micros_time_base = micros();
+
+    while(micros_time < micros_time_base + 2000)
+    {
+        if(micros_time > micros_time_base + motor_upper_left_us)
+            digitalWrite(8, LOW);
+        if(micros_time > micros_time_base + motor_upper_right_us)
+            digitalWrite(9, LOW);
+        if(micros_time > micros_time_base + motor_lower_left_us)
+            digitalWrite(10, LOW);
+        if(micros_time > micros_time_base + motor_lower_right_us)
+            digitalWrite(11, LOW);
+
+        micros_time = micros();
+        if(debug) Serial.print("micros = ");
+        if(debug) Serial.println(micros_time);
     }
+}
+
+// void setValues(double p1, double p2, double p3, double p4) // range [0; 100]
+// {
+//     motor_upper_left_us = calcMicroseconds(p1);
+//     motor_upper_right_us = calcMicroseconds(p2);
+//     motor_lower_left_us = calcMicroseconds(p3);
+//     motor_lower_right_us = calcMicroseconds(p4);
+// }
+
+void Update_Motor_Values()
+{// Read MPU data
+        digitalWrite(DEBUG_PIN, 1);
+        
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        mpu.dmpGetQuaternion(&quater, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &quater);
+        mpu.dmpGetYawPitchRoll(ypr, &quater, &gravity);
+        pitchInput = (ypr[1] * 180/M_PI);
+        rollInput = (ypr[2] * 180/M_PI);
+
+        if(debug) Serial.print("PR:");
+        if(debug) Serial.print(pitchInput);
+        if(debug) Serial.print(",");
+        if(debug) Serial.println(rollInput);
+       
+
+        //Update Pids
+        pitchPID.Compute();
+        rollPID.Compute();
+
+        // Apply PID output to motor PWM
+        double motor_upper_left = speedRequest + (rollOutput / 2) + (pitchOutput / 2);
+        double motor_upper_right = speedRequest - (rollOutput / 2) + (pitchOutput / 2);
+        double motor_lower_left = speedRequest + (rollOutput / 2) - (pitchOutput / 2);
+        double motor_lower_right = speedRequest - (rollOutput / 2) - (pitchOutput / 2);
+
+        if(motor_upper_left < 0) motor_upper_left = 0;
+        if(motor_upper_right < 0) motor_upper_right = 0;
+        if(motor_lower_left < 0) motor_lower_left = 0;
+        if(motor_lower_right < 0) motor_lower_right = 0;
+
+        // if(debug) Serial.print("setValues(");
+        // if(debug) Serial.print(motor_upper_left);
+        // if(debug) Serial.print(",");
+        // if(debug) Serial.print(motor_upper_right);
+        // if(debug) Serial.print(",");
+        // if(debug) Serial.print(motor_lower_left);
+        // if(debug) Serial.print(",");
+        // if(debug) Serial.print(motor_lower_right);
+        // if(debug) Serial.println(")");
+        
+        // Write motor PWM
+        setValues(motor_upper_left, motor_upper_right, motor_lower_left, motor_lower_right);
+        mpuInterrupt = false;
+        
+        digitalWrite(DEBUG_PIN, 0);
+}
 
 void loop() 
 {
-    digitalWrite(13, blinkState);
-    blinkState = !blinkState;
+    // if(debug) Serial.println(F("if (!dmpReady)"));
+    
+    if (!dmpReady)  return;
 
-    // Serial.println(F("if (!dmpReady)"));
-    if (!dmpReady) 
-        return;
+    // if(debug) Serial.println(F("readRcControllerData"));
 
-    // Serial.println(F("readRcControllerData"));
-
-    // Get data from Bluetooth
+    // Get command from Bluetooth
     readRcControllerData();
 
     bool isAllowedEngineUpdateByPid = true;
@@ -190,110 +280,14 @@ void loop()
         clearControlRequestValues();
     }
 
-    // Serial.print(F("while (!mpuInterrupt && fifoCount < packetSize)"));
-    // Serial.print(mpuInterrupt);
-    // Serial.println(fifoCount);
+    if(mpuInterrupt)
+        Update_Motor_Values();
+
+    // if(debug) Serial.print(F("while (!mpuInterrupt && fifoCount < packetSize)"));
+    // if(debug) Serial.print(mpuInterrupt);
+    // if(debug) Serial.println(fifoCount);
     
-    while (!mpuInterrupt && fifoCount < packetSize) 
-    {
-        static bool st = false;
-        st = !st;
-        digitalWrite(12, st);
-
-        if (!isMpuDataActual())
-        {
-            shutDownQ();
-            clearControlRequestValues();
-            return;
-        }
-
-        if (!isAllowedEngineUpdateByPid)
-            continue;
-
-        float motor_upper_left = speedRequest + (rollOutput / 2) + (pitchOutput / 2);
-        float motor_upper_right = speedRequest - (rollOutput / 2) + (pitchOutput / 2);
-        float motor_lower_left = speedRequest + (rollOutput / 2) - (pitchOutput / 2);
-        float motor_lower_right = speedRequest - (rollOutput / 2) - (pitchOutput / 2);
-
-        motor_upper_left = limit_power(motor_upper_left);
-        motor_upper_right = limit_power(motor_upper_right);
-        motor_lower_left = limit_power(motor_lower_left);
-        motor_lower_right = limit_power(motor_lower_right);
-
-        // Serial.println(F("setValues(p1, p2, p3, p4);"));
-
-        setValues(motor_upper_left, motor_upper_right, motor_lower_left, motor_lower_right);
-    }
-
-    mpuInterrupt = false;
-
-    // Serial.println(F("mpu.getIntStatus"));
-    mpuIntStatus = mpu.getIntStatus();
-
-    // Serial.println(F("mpu.getFIFOCount"));
-    fifoCount = mpu.getFIFOCount();
-
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) 
-    {
-        // Serial.println(F("mpu.resetFIFO();"));
-        mpu.resetFIFO();
-        // Serial.println(F("FIFO overflow!"));
-    } 
-    else 
-    {
-        // Serial.println(F("if ((mpuIntStatus & 0x02) > 0)"));
-        if ((mpuIntStatus & 0x02) > 0) 
-        {
-            // Serial.println(F("while (fifoCount < packetSize) "));
-            
-            constexpr uint32_t timeout = 100;
-            uint32_t t1 = millis();
-            while (fifoCount < packetSize)
-            {
-                fifoCount = mpu.getFIFOCount();
-                if (millis() - t1 > timeout)
-                    break;
-            }
-            
-            // Serial.println(F("mpu.getFIFOBytes(fifoBuffer, packetSize);"));
-            lastMpuReaded = millis();
-
-            mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-            fifoCount -= packetSize;
-
-            mpu.dmpGetQuaternion(&quater, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &quater);
-            mpu.dmpGetYawPitchRoll(ypr, &quater, &gravity);
-            pitchInput = (ypr[1] * 180/M_PI);
-            rollInput = (ypr[2] * 180/M_PI);
-
-            //Update Pids
-            pitchPID.Compute();
-            rollPID.Compute();
-
-            #ifdef DEBUG_OUTPUT
-                Serial.print("gravity (x, y, z): ");
-                Serial.print(gravity.x);
-                Serial.print(" ");
-                Serial.print(gravity.y);
-                Serial.print(" ");
-                Serial.print(gravity.z);
-
-                Serial.print("   (pitchIn, rollIn): ");
-                Serial.print(pitchInput);
-                Serial.print(" ");
-                Serial.print(rollInput);
-
-                Serial.print("   (pitchOut, rollOut): ");
-                Serial.print(pitchOutput);
-                Serial.print(" ");
-                Serial.print(rollOutput);
-            #endif
-        }
-    // Serial.println(F("end"));
-    }
-// Serial.println(F("END"));
+    
 }
 
 void clearControlRequestValues()
@@ -311,7 +305,8 @@ bool isMpuDataActual()
 {
     if (millis() - lastMpuReaded > MaxTimeWaitingForMpuData)
         return false;
-    return true;
+    else
+        return true;
 }
 
 bool isShutDownRequested()
@@ -328,14 +323,6 @@ bool isShutDownRequested()
     return false;
 }
 
-float limit_power(float p)
-{
-    if(p < 0) p = 0;
-    if(p > 100) p = 100;
-
-    return p;
-}
-
 void waitData(int data)
 {
     while (true)
@@ -348,31 +335,31 @@ void readRcControllerData()
 {
     if (!rcCheckStatus)
     {
-        Serial.println(F("For Calibration send 'k', to skip anysing else"));
+        if(debug) Serial.println(F("For Calibration send 'k', to skip anysing else"));
         while (true)
         {
             if(Serial1.available() > 0)
             {
                 if (Serial1.read() == 'k')
                 {
-                    Serial.println(F("Disconnect engine power, then send 'k'"));
+                    if(debug) Serial.println(F("Disconnect engine power, then send 'k'"));
                     waitData('k');
                     setValues(100, 100, 100, 100);
-                    Serial.println(F("Connect engine power, wait for signal, then send 'k'"));
+                    if(debug) Serial.println(F("Connect engine power, wait for signal, then send 'k'"));
                     waitData('k');
                     setValues(0, 0, 0, 0);
-                    Serial.println(F("Calibration ended, send 'k'"));
+                    if(debug) Serial.println(F("Calibration ended, send 'k'"));
                     waitData('k');
                 }
             break;
             }
         }
 
-        Serial.println(F("Wait 8 then 9 symbols to init RC controller"));
+        if(debug) Serial.println(F("Wait 8 then 9 symbols to init RC controller"));
         waitData('8');
         waitData('9');
         rcCheckStatus = true;
-        Serial.println(F("RC controller checked"));
+        if(debug) Serial.println(F("RC controller checked"));
     }
 
     //TODO: update to use sinle custom check data protocol
